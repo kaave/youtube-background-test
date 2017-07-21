40016db5fe3896eefe0235af81d35a589b8f925f
kaave <abe@framelunch.jp>
2017-07-22 01:50:30 +0900

    とりあえず並べた

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,64 +1,61 @@
 import 'babel-polyfill';  // アプリ内で1度だけ読み込む エントリーポイントのてっぺん推奨
-import $ from 'jquery';
-import touchEvents from 'jquery-touch-events';
-import anime from 'animejs';
+import YouTubePlayer from 'youtube-player';
 
-import notice from 'libraries-frontend-framelunch/js/notice';
-import state from 'libraries-frontend-framelunch/js/state';
-import subscribeEvents from './_events';
+const videoIDs = [
+  'Cnj64DsO8T8',
+  '6MK2TKFFtQA',
+  'BAXQ6HaRG8s',
+  'fTyewgmEoGU',
+  'kzjMI00A1f8',
+  'vfJAaFaxqiE',
+  '3NZGbD236fw',
+];
 
-class Main {
-  constructor() {
-    this.setTouchEventsToJQuery();
-    this.onDOMContentLoaded = this.onDOMContentLoaded.bind(this);
-    this.onWindowResize = this.onWindowResize.bind(this);
-    this.onScrollTop = this.onScrollTop.bind(this);
-  }
-
-  setTouchEventsToJQuery() {
-    touchEvents($);
-  }
-
-  onDOMContentLoaded() {
-    anime({
-      targets: '#test-img',
-      translateX: [
-        { value: 100, duration: 1200 },
-        { value: 0, duration: 800 },
-      ],
-      rotate: '1turn',
-      backgroundColor: [
-        { value: `hsl(${Math.floor(Math.random() * 360)}, 90%, 70%)`, dulation: 500 },
-        { value: `hsl(${Math.floor(Math.random() * 360)}, 90%, 70%)`, dulation: 500 },
-        { value: `hsl(${Math.floor(Math.random() * 360)}, 90%, 70%)`, dulation: 500 },
-        { value: `hsl(${Math.floor(Math.random() * 360)}, 90%, 70%)`, dulation: 500 },
-      ],
-      duration: 2000,
-      loop: false,
+window.addEventListener('DOMContentLoaded', () => {
+  videoIDs.forEach((videoId, i) => {
+    const player = YouTubePlayer(`video-${i + 1}`, {
+      videoId,
+      playerVars: {
+        loop: 1,
+        controls: 0,
+        autoplay: 1,
+        showinfo: 0
+      },
+      suggestedQuality: 'small'
     });
-  }
+    player.mute();
+  });
+  // // YouTubeの埋め込み
+  // function onYouTubeIframeAPIReady() {
+  //   ytPlayer = new YT.Player(
+  //     'youtube', // 埋め込む場所をIDで設定
+  //     {
+  //       videoId: 'vk2XlND2kQ0', // YouTubeのIDを指定
+  //       width: 640,
+  //       height: 400,
+  //       playerVars: {
+  //         controls: 0,
+  //         autoplay: 1,
+  //         showinfo: 0,
+  //       },
+  //       events: {
+  //         onReady: onPlayerReady,
+  //         onStateChange: onPlayerStateChange
+  //       }
+  //     }
+  //   );
+  // }
 
-  onWindowResize($window) {
-    console.log('onWindowResize:', $window.width(), $window.height());
-  }
+  // function onPlayerReady(event) {
+  //   event.target.playVideo();
+  //   event.target.mute();
+  // }
 
-  onScrollTop(scrollTop) {
-    console.log('scrollTop:', scrollTop);
-  }
-}
-
-const main = new Main();
-window.addEventListener('DOMContentLoaded', main.onDOMContentLoaded);
-notice.listen('resize', main.onWindowResize);
-notice.listen('scroll', main.onScrollTop);
-subscribeEvents();
-
-/*
- * 以下テスト用
- */
-notice.listen('init', data => console.log(data));
-notice.publish('init', [123]);
-state.listen('add:a', v => console.log('add:a', v));
-state.listen('add:c', v => console.log('add:c', v));
-state.change('a/b/c', [5678]);
-
+  // function onPlayerStateChange(e) {
+  //   const ytStatus = e.target.getPlayerState();
+  //   if (ytStatus === YT.PlayerState.ENDED) {
+  //     ytPlayer.playVideo();
+  //     ytPlayer.mute();
+  //   }
+  // }
+});
